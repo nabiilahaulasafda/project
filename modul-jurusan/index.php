ad146061b30807f69d346a85459c7cb431461d9e
nabiilahaulasafda <nabiilahaulasafdaa@gmail.com>
2024-11-13 15:17:56 +0700

    memperbaiki link navbar dan membuat index jurusan

---
+++ modul-jurusan/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>data jurusan </title>
+    <link rel="stylesheet" href="../css/bootstrap.css">
+</head>
+<body>
+<?php include_once('../navbar.php') ?>
+
+<div class="container">
+    <div class="row mt-5">
+      <div class="col-6 m-auto">
+      <div class="card">
+  <div class="card-header bg-dark-subtle">
+    <h3> Data Jurusan </h3>
+  </div>
+
+  <div class="card-body">
+    <h4> Selamat Datang, dan Selamat Membaca</h4>
+    <p> Semoga Hari anda baik selalu ketika membaca halaman ini </p>
+  </div>
+</div>
+      </div>
+    </div>
+  </div>
+
+    <script src="../js/bootstrap.js"></script>
+    <script src="../js/bootstrap.bundle.js"></script>
+</body>
+</html>
