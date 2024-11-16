168008cf0d83396b4a5f3971ef7dfbc85e2542c3
nabiilahaulasafda <nabiilahaulasafdaa@gmail.com>
2024-11-16 11:25:15 +0700

    menambhkan hapus dan menambahkan tombol modal

--- modul-jurusan/index.php
+++ modul-jurusan/index.php
@@ -49,7 +49,32 @@
                     <td> <?=$data['jurusan']?> </td>
                     <td>
                         <a class="btn btn-info btn-sm" href="" > <i class="fa-solid fa-pen-to-square"></i> </a>
-                        <a class="btn btn-danger btn-sm" href="" > <i class="fa-solid fa-trash"></i> </a>
+
+                        <!-- Button trigger modal -->
+                        <button type="button" class="btn btn-danger btn-sm" data-bs-toggle="modal" data-bs-target="#hapus<?=$data['id']?>">
+                        <i class="fa-solid fa-trash"></i>
+                        </button>
+
+                        <!-- Modal -->
+                        <div class="modal fade" id="hapus<?=$data['id']?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                            <div class="modal-header">
+                                <h1 class="modal-title fs-5" id="exampleModalLabel">PERINGATAN !!!</h1>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                                Yakin Ingin Menghapus Data <b><?=$data['jurusan']?> ? 
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
+                                <a href="hapus.php?xyz=<?=$data['id']?>" class="btn btn-danger">Hapus</a>
+
+                            </div>
+                            </div>
+                        </div>
+                        </div>
+
                     </td>
                 </tr>
 
