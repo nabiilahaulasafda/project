f1a9adf6315e336671e416f614b2cef4f9f5b29a
Necma13 <nabiilahaulasafdaa@gmail.com>
2024-12-21 09:56:48 +0700

    form login

--- login.php
+++ login.php
@@ -1,3 +1,29 @@
+<?php
+if (isset($_POST['tombol'])) {
+    // 1.koneksi ke database
+    include_once("koneksi.php");
+
+    // 2.mengambil value dari input
+    $email = $_POST['email'];
+    $_pass = $_POST['pass'];
+
+    // 3.tulisakn query pengecekan apakah data login tersedia di database
+    $sql_cek = "SELECT * FROM users WHERE email='$email' AND password='$pass'";
+
+    // 4.menjalankan query diatas
+    $qry_cek = mysqli_query($koneksi, $sql_cek);
+
+    // 5.pengecekan lanjutan
+    $cek = mysqli_num_rows($qry_cek);
+
+    // 6.buatkan if jika login berhasil atau gagal
+    if ($cek > 0) {
+        // login berhasil
+    } else {
+        // login gagal
+    }
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -19,21 +45,24 @@
                         <h3>Sistem Informasi Mahasiswa</h3>
                     </div>
                     <div class="card-body">
-                        <form>
+                        <form method="post" action="login.php">
                             <div class="mb-3">
                                 <label for="exampleInputEmail1" class="form-label">Email address</label>
-                                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                                <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+
                             </div>
+
                             <div class="mb-3">
                                 <label for="exampleInputPassword1" class="form-label">Password</label>
-                                <input type="password" class="form-control" id="exampleInputPassword1">
+                                <input type="password" name="pass" class="form-control" id="exampleInputPassword1">
                             </div>
+
                             <div class="mb-3 form-check">
-                                <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                                <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                                <input type="checkbox" name="cek" value="yes" class="form-check-input" id="exampleCheck1">
+                                <label class="form-check-label" for="exampleCheck1">Ingat Saya</label>
                             </div>
-                            <button type="submit" class="btn btn-primary">Submit</button>
+
+                            <button type="submit" name="tombol" class="btn btn-primary">Login</button>
                         </form>
                     </div>
                 </div>
